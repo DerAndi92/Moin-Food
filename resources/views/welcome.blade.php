4f5feb1d238670919fa37fb3e0f47a256e8b56f8
Marilena Jens <marilena.jens@haw-hamburg.de>
2018-05-18 11:29:17 +0200

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel sfdfc
                 </div>
 
                 <div class="links">
