8eae749f274f3f326029fe9b99150d39da3289c8
Marilena Jens <marilena.jens@haw-hamburg.de>
2018-06-06 10:52:50 +0200

    Frontend

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,21 +4,22 @@
         <h1>Essen. Trinken. Wo du willst.</h1>
         <input id="search-input" type="text" class="input_text" tabindex="1" placeholder="Gib deinen Standort ein" />
         <button type="button" class="start-suche">Los geht's!</button>
+
         <div class="search-options">
             <select name="typ">
-                <option value="0">Typ</option>
+                <option class="icon-down-open" value="0">Typ</option>
             </select>
             <select name="anlass">
-                <option value="0">Anlass</option>
+                <option class="icon-down-open" value="0">Anlass</option>
             </select>
             <select name="preiskategorie">
-                <option value="0">Preiskategorie</option>
+                <option class="icon-down-open" value="0">Preiskategorie</option>
             </select>
             <select name="art">
-                <option value="0">Art</option>
+                <option class="icon-down-open" value="0">Art</option>
             </select>
             <select name="altersspanne">
-                <option value="0">Altersspanne</option>
+                <option class="icon-down-open" value="0">Altersspanne</option>
             </select>
         </div>
 </form>
