21ca51b2f91565d68577502af38eb486d196e04f
Marilena Jens <marilena.jens@haw-hamburg.de>
2018-06-09 13:21:14 +0200

    Favicon, About-Text (Logo für die Mitte)

--- resources/views/elements/about.blade.php
+++ resources/views/elements/about.blade.php
@@ -1,6 +1,8 @@
 <section id="about-us">
     <div class="container">
         <h2>Warum es Moin-Food gibt.</h2>
-        <p>Finden Sie die besten Lokale in Hamburg. Passend auf Ihre Wünsche und den Anlass des Besuchs. 40 Lokale – ob Streetfood, Eisdielen, Bars oder Restaurants der gehobenen Klasse</p>
+        <p>Wir lieben Essen. Und das am besten in einem tollen Lokal, mit leckeren Getränken und der passenden Atmosphäre. Wenn man erstmal seine Lieblingslokale kennengerlernt hat, weicht man ungern auf andere Restaurants ab. Schließlich weiß man vorher nicht, ob es einem dort auch so gut gefällt: Ist der Service freundlich? – Wie ist die Preis-Leistung? – Gibt es Mittagsangebote?<br>Deshalb haben wir viele Lokale in Hamburg besucht und helfen euch mit 'Moin-Food', ein passendes Lokal in eurer Nähe zu finden.
+            Ob Streetfood, Eisdielen, Bars oder Restaurants der gehobenen Klasse. Wir möchten, dass ihr für euren Anlass und Geschmack das richtige findet und das möglichst einfach und verlässlich.
+        </p>
     </div>
 </section>
