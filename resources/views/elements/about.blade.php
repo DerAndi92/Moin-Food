ff1a077f6a5222648bc3c84025ac351499577fa4
Marilena Jens <marilena.jens@haw-hamburg.de>
2018-06-20 10:20:14 +0200

    Text

--- resources/views/elements/about.blade.php
+++ resources/views/elements/about.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <h2>Warum es Moin-Food gibt.</h2>
         <p>Moin ihr Lüd! Wir lieben Essen. Und das am besten in einem tollen Lokal mit leckeren Getränken und der passenden Atmosphäre. Hat man jedoch erstmal seine Lieblingslokale kennengerlernt, weicht man ungern auf andere Restaurants ab. Schließlich weiß man vorher nicht, ob es einem dort auch so gut gefällt: Ist der Service freundlich? – Wie ist das Preis-Leistungs-Verhältnis? – Gibt es Mittagsangebote?<br>Deshalb ist unsere Mission, euch dabei zu helfen, ausgewählte Lokale als neue Lieblingsplätze zu entdecken. Mit "Moin-Food Hamburg" bekommt ihr passende Lokale in eurer Nähe vorgeschlagen.
-            Ob Streetfood, Eisdielen, Bars oder Restaurants der gehobenen Klasse. Wir möchten, dass ihr für euren Anlass und Geschmack das richtige findet.
+            Ob Streetfood, Eisdielen, Bars oder Restaurants der gehobenen Klasse.<br>Wir möchten, dass ihr für euren Anlass und Geschmack das richtige findet.
         </p>
     </div>
 </section>
